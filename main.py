554c15672b4ded032de5ad08615b994836638fd4
Eugene <72232749+EugeneIm@users.noreply.github.com>
2021-06-24 18:17:14 -0700

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from car import car
-
-car1 = Car("Honda", "Civic", 2020, 15000.0, 20000)
-print(car1.get_details())
-print("First Car’s Initial Profit is $%.2f" % car1.calc_profit())
-car1.reduce_price(1000)
-print("First Car’s New Profit is $%.2f" % car1.calc_profit())
